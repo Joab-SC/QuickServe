e423f0cfc22c9a861bc659fdc8991811193f77ec
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-05-11 19:01:28 -0500

    chore: Create git repository

---
+++ src/test/java/co/edu/uniquindio/quickserve/QuickServeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.quickserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
