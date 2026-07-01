fd178095dbac2304626a725d70e3f74a75120b65
Joab-SC <142185992+Joab-SC@users.noreply.github.com>
2026-07-01 14:12:51 -0500

    refactor: Use lombok in DetallePedido

--- src/main/java/co/edu/uniquindio/quickserve/model/DetallePedido.java
+++ src/main/java/co/edu/uniquindio/quickserve/model/DetallePedido.java
@@ -1,5 +1,14 @@
 package co.edu.uniquindio.quickserve.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetallePedido {
 
     private Integer id;
@@ -7,33 +16,9 @@ public class DetallePedido {
     private Producto producto;
     private Integer cantidad;
 
-    public DetallePedido() {}
-
-    public DetallePedido(Integer id, Pedido pedido, Producto producto, Integer cantidad) {
-        this.id = id;
-        this.pedido = pedido;
-        this.producto = producto;
-        this.cantidad = cantidad;
-    }
-
     public Double getSubtotal() {
         if (producto == null || cantidad == null) return 0.0;
         return producto.getPrecio() * cantidad;
     }
 
-    // getters y setters
-    public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-
-    public Pedido getPedido() { return pedido; }
-    public void setPedido(Pedido pedido) { this.pedido = pedido; }
-
-    public Producto getProducto() { return producto; }
-    public void setProducto(Producto producto) { this.producto = producto; }
-
-    public Integer getCantidad() { return cantidad; }
-    public void setCantidad(Integer cantidad) { this.cantidad = cantidad; }
-    public String getSubtotalFormateado() {
-        return String.format("$%,.0f", getSubtotal());
-    }
 }
